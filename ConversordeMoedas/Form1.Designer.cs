6a5646fd48bb288990b750db001bd70be28a5754
maath.j <mateua12392@gmail.com>
2024-09-21 15:54:37 -0300

    Adição da Lógica

--- ConversordeMoedas/Form1.Designer.cs
+++ ConversordeMoedas/Form1.Designer.cs
@@ -28,12 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(284, 288);
+            button1.Margin = new Padding(4, 4, 4, 4);
+            button1.Name = "button1";
+            button1.Size = new Size(153, 62);
+            button1.TabIndex = 0;
+            button1.Text = "Calcular";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(40, 104);
+            textBox1.Margin = new Padding(4, 4, 4, 4);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(128, 29);
+            textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(40, 78);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 21);
+            label1.TabIndex = 2;
+            label1.Text = "Valor";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(297, 78);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(63, 21);
+            label2.TabIndex = 4;
+            label2.Text = "Moeda";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(297, 104);
+            textBox2.Margin = new Padding(4, 4, 4, 4);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(128, 29);
+            textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(564, 78);
+            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(115, 21);
+            label3.TabIndex = 6;
+            label3.Text = "Valor em Real";
+            label3.Click += label3_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(564, 104);
+            textBox3.Margin = new Padding(4, 4, 4, 4);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(128, 29);
+            textBox3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(17, 13);
+            label4.Margin = new Padding(4, 0, 4, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(283, 21);
+            label4.TabIndex = 7;
+            label4.Text = "Conversor de Dolar e Euro para Real";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(726, 449);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(textBox3);
+            Controls.Add(label2);
+            Controls.Add(textBox2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            Margin = new Padding(4, 4, 4, 4);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox3;
+        private Label label4;
     }
 }
--- ConversordeMoedas/Form1.cs
+++ ConversordeMoedas/Form1.cs
@@ -6,5 +6,43 @@ namespace ConversordeMoedas
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double valor, valoreal, euro, dolar;
+
+            euro = 6.16;
+            dolar = 5.52;
+            string moeda = textBox2.Text;
+            valor = double.Parse(textBox1.Text);
+
+            if (moeda.ToLower() == "euro")
+            {
+                valoreal = valor * euro;
+                textBox3.Text = valoreal.ToString("C");
+            } else if(moeda.ToLower() == "dolar")
+            {
+                valoreal = valor * dolar;
+                textBox3.Text = valoreal.ToString("C");
+            }
+            else
+            {
+                MessageBox.Show("Moeda invalida, insira Euro ou Dolar.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox3.Text = string.Empty;
+            }
+
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+        
+
+        }
     }
 }
